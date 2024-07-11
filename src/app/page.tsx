e7cb736522b70e2dda43dfd98b2faa3d7b7c375b
Balasainath17 <balasainath17@gmail.com>
2024-07-12 01:59:46 +0530

    added next-auth and itemstable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,42 @@
-import { bids as bidsSchema } from "@/db/schema";
+import { items } from "@/db/schema";
 import { database } from "@/db/database";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { revalidatePath } from "next/cache";
+import { SignIn } from "@/components/sign-in";
+import { SignOut } from "@/components/sign-out";
+import { auth } from "@/auth";
 
 export default async function HomePage() {
-  const bids = await database.query.bids.findMany();
+const session = await auth();
 
-  return <main className="container mx-auto py-12">
+
+
+const allItems = await database.query.items.findMany();
+
+  return (
+     <main className="container mx-auto py-12">
+     {session? <SignOut/> : <SignIn/>}
+     
+     {session?.user?.name}
+
       <form 
         action={async(formData: FormData) => {
           "use server";
-          await database.insert(bidsSchema).values({});
+          await database.insert(items).values({
+            name: formData.get("name") as string,
+            userId: session?.user?.id!,
+          });
           revalidatePath("/");
         }} >
-          <Input name="bid" placeholder="Bid" />
-          <Button type="submit">Place Bid</Button>
+          <Input name="name" placeholder="Name your item" />
+          <Button type="submit">Post Item</Button>
+
         </form>
 
-        {bids.map(bid => (
-          <div key= {bid.id}>{bid.id}</div>
-        ))}
-
-  </main>;
+        {allItems.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+  </main>
+  );
 }
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,78 @@
 
 import {
+  integer,
   pgTable,
+  primaryKey,
   serial,
+  text,
+  timestamp,
 } from "drizzle-orm/pg-core";
+import { AdapterAccount } from "next-auth/adapters";
 
+export const users = pgTable("bidder_user", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: text("name"),
+  email: text("email").notNull(),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  image: text("image"),
+});
+ 
+export const accounts = pgTable(
+  "bidder_account",
+  {
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccount>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  })
+);
+ 
+export const sessions = pgTable("bidder_session", {
+  sessionToken: text("sessionToken").primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: timestamp("expires", { mode: "date" }).notNull(),
+});
+ 
+export const verificationTokens = pgTable(
+  "bidder_verificationToken",
+  {
+    identifier: text("identifier").notNull(),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+  },
+  (verificationToken) => ({
+    compositePk: primaryKey({
+      columns: [verificationToken.identifier, verificationToken.token],
+    }),
+  })
+);
 
 export const bids = pgTable("bidder_bids", {
   id: serial("id").primaryKey(),
 });
+
+
+export const items = pgTable("bidder_items",{
+  id: serial("id").primaryKey(),
+  userId: text("userId").notNull().references(() => users.id, { onDelete: "cascade" }),
+  name: text("name").notNull(),
+
+} );
